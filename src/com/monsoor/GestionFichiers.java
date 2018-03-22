2ec82ef7b269984344faa0af7637cd13de8c85f7
mmonsoo <misharl.monsoor@gmail.com>
2018-03-22 11:25:52 +0100

    Deuxième commit

--- src/com/monsoor/GestionFichiers.java
+++ src/com/monsoor/GestionFichiers.java
@@ -1,10 +1,57 @@
 package com.monsoor;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class GestionFichiers {
 
 	public static void main(String[] args) {
+		GererFichier();
+		GererBinaire();
+}
+
+	/**
+	 * 
+	 */
+	private static void GererBinaire() {
+		//Les flux binaires
+				
+				FileInputStream fi = null;
+				try {
+					fi= new FileInputStream("C:\\Users\\ACI\\Documents\\test.bin.txt");
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				int n = 0;
+				//byte[] buf = new byte[8];
+				do {
+					try {
+						//On lit des bytes et c'est en code ASCII
+						n=fi.read();
+						System.out.println();
+		
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					//Some stuff
+				} 
+				while(n!=-1);
+				try {
+					fi.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	}
+
+	/**
+	 * 
+	 */
+	private static void GererFichier() {
 		// TODO Auto-generated method stub
 		String root=System.getProperty("user.dir");
 		System.out.println(root);
@@ -14,7 +61,6 @@ public class GestionFichiers {
 		if(!f.exists()) {
 			f.mkdir();
 		}
-
 	}
 
 }
